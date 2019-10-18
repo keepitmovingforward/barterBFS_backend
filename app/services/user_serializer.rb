979b635cc3c146fad393021a1ee71eabc856d520
icydwei <robpaik@gmail.com>
2019-10-18 10:58:37 -0400

    updated user serializer

--- app/services/user_serializer.rb
+++ app/services/user_serializer.rb
@@ -6,7 +6,7 @@ class UserSerializer < ApplicationController
 
     def to_serialized_json
       @user.to_json({
-        :include => {items: {except: [:created_at, :updated_at, :user_id]},
+        :include => {items: {except: [:created_at, :updated_at]},
                     sentBarters: {except: [:created_at, :updated_at, :sender_id]},
                     recdBarters: {except: [:created_at, :updated_at, :recipient_id]}
                   },
